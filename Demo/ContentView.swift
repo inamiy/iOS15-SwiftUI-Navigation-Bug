4bbe982fbe0cb92c2ff910e553fb2bd262646b64
Yasuhiro Inami <inamiy@gmail.com>
2021-09-24 11:00:22 +0900

    Add Sources

---
+++ Demo/ContentView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct AppView: View {
+    @StateObject
+    var viewModel: ViewModel = .init()
+
+    var body: some View {
+        NavigationView {
+            ContentView(level: 0)
+                .environmentObject(viewModel)
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @EnvironmentObject
+    var viewModel: ViewModel
+
+    var level: Int
+
+    var body: some View {
+        List {
+            NavigationLink(
+                destination: ContentView(level: level + 1)
+                    .environmentObject(viewModel),
+                isActive: $viewModel.state.isActives[level],
+                label: {
+                    Text("Navigate")
+                }
+            )
+        }
+    }
+}
+
+/// Behaves as Single-Source-of-Truth, passed as `EnvironmentObject`.
+class ViewModel: ObservableObject {
+    @Published
+    var state: State = .init()
+
+    struct State {
+        /// Array of `isActive` flags for each navigation stack (centralized management as single-source-of-truth).
+        var isActives: [Bool] = [Bool](repeating: false, count: 5) {
+            didSet {
+                print("===>", isActives)
+            }
+        }
+    }
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct iOS15_SwiftUINavigationBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
